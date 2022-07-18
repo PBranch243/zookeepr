825f48308e1ccad8fafc0ef8a76e9f5c84ff5352
Phil Branch <philbranch243@gmail.com>
2022-07-18 17:58:40 -0500

    lkjlkj;lkj

--- server.js
+++ server.js
@@ -65,6 +65,22 @@ function createNewAnimal(body, animalsArray) {
   return animal;
 };
 
+function valiateAnimal(animal){
+  if (!animal.name || typeof animal.name !== 'string'){
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string'){
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string'){
+    return false
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+    return false;
+  }
+  return true;
+};
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -85,11 +101,14 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
-
+  //if any data in req.body is incorrect, send 400 error back
+  if (!valiateAnimal(req.body)){
+    res.status(400).send('The animal is not properly formatted.');
+  }else{
   //add animal to json file and animals array in this funcion
   const animal = createNewAnimal(req.body, animals);
-
   res.json(req.body);
+  }
 });
 
 app.listen(PORT, () => {
