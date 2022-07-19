4f4d1afba9061440fd9af6ea40b3a33bd8607a64
Phil Branch <philbranch243@gmail.com>
2022-07-19 10:53:31 -0500

    refactor done

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,24 +1,20 @@
 const path = require('path');
 const router = require('express').Router();
 
-//send homepage to browser
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-//send animals page to browser
 router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
 });
 
-//send zookeeper page to browser
 router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
 
-//add a wildcard route to redirect non-existant routes to homepage
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 //make public folder accessible so html can acces its css and javascript
 app.use(express.static('public'));
@@ -13,59 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+//use the new api routes after refactor
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/api/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
 
-app.get('/api/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-app.post('/api/animals', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
-  //if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)){
-    res.status(400).send('The animal is not properly formatted.');
-  }else{
-  //add animal to json file and animals array in this funcion
-  const animal = createNewAnimal(req.body, animals);
-  res.json(req.body);
-  }
-});
-
-//send homepage to browser
-app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-//send animals page to browser
-app.get('/animals',(req,res)=>{
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-//send zookeeper page to browser
-app.get('/zookeepers',(req,res)=>{
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-//add a wildcard route to redirect non-existant routes to homepage
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`API Server now on port ${PORT}!`);
 });
-
-// console.log (animals);
